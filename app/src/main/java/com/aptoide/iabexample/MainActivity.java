9aa47d082f4f7a1f252626712a3e8bd6a0c9bf55
Joao Raimundo <joao.raimundo@aptoide.com>
2018-07-16 11:42:20 +0100

    1- Added Appcoins wallet aidl for ipc on IAB flow. 2- Updated several variable with reference to asf and aptoide. 3 - Added iab helper and main activity flow for iab flow.

--- app/src/main/java/com/aptoide/iabexample/MainActivity.java
+++ app/src/main/java/com/aptoide/iabexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,8 +15,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.aptoide.iabexample.util.IabBroadcastReceiver;
+import com.aptoide.iabexample.util.IabHelper;
+import com.aptoide.iabexample.util.IabResult;
+import com.aptoide.iabexample.util.Inventory;
+import com.aptoide.iabexample.util.Purchase;
 import com.asf.appcoins.sdk.iab.payment.PaymentDetails;
 import com.asf.appcoins.sdk.iab.payment.PaymentStatus;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import java.util.ArrayList;
 import java.util.List;
@@ -73,7 +80,8 @@ import static com.aptoide.iabexample.Application.appCoinsIab;
  * we have to apply its effects to our world and consume it. This
  * is also very important!
  */
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity
+    implements IabBroadcastReceiver.IabBroadcastListener, OnClickListener {
   // Debug tag, for logging
   static final String TAG = "TrivialDrive";
   // How many units (1/4 tank is our unit) fill in the tank.
@@ -96,6 +104,109 @@ public class MainActivity extends Activity implements OnClickListener {
   String mSelectedSubscriptionPeriod = "";
   // Current amount of gas in tank, in units
   int mTank;
+  // The helper object
+  IabHelper mHelper;
+  // Provides purchase notification while this app is running
+  IabBroadcastReceiver mBroadcastReceiver;
+
+  // Listener that's called when we finish querying the items and subscriptions we own
+  IabHelper.QueryInventoryFinishedListener mGotInventoryListener =
+      new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+          Log.d(TAG, "Query inventory finished.");
+
+          // Have we been disposed of in the meantime? If so, quit.
+          if (mHelper == null) return;
+
+          // Is it a failure?
+          if (result.isFailure()) {
+            complain("Failed to query inventory: " + result);
+            return;
+          }
+
+          Log.d(TAG, "Query inventory was successful.");
+
+          /*
+           * Check for items we own. Notice that for each purchase, we check
+           * the developer payload to see if it's correct! See
+           * verifyDeveloperPayload().
+           */
+
+          // Do we have the premium upgrade?
+          Purchase premiumPurchase = inventory.getPurchase(Skus.SKU_PREMIUM_ID);
+          mIsPremium = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
+          Log.d(TAG, "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
+
+          // First find out which subscription is auto renewing
+          Purchase gasMonthly = inventory.getPurchase(Skus.SKU_INFINITE_GAS_MONTHLY_ID);
+          Purchase gasYearly = inventory.getPurchase(Skus.SKU_INFINITE_GAS_YEARLY_ID);
+          if (gasMonthly != null && gasMonthly.isAutoRenewing()) {
+            mInfiniteGasSku = Skus.SKU_INFINITE_GAS_MONTHLY_ID;
+            mAutoRenewEnabled = true;
+          } else if (gasYearly != null && gasYearly.isAutoRenewing()) {
+            mInfiniteGasSku = Skus.SKU_INFINITE_GAS_YEARLY_ID;
+            mAutoRenewEnabled = true;
+          } else {
+            mInfiniteGasSku = "";
+            mAutoRenewEnabled = false;
+          }
+
+          // The user is subscribed if either subscription exists, even if neither is auto
+          // renewing
+          mSubscribedToInfiniteGas = (gasMonthly != null && verifyDeveloperPayload(gasMonthly)) || (
+              gasYearly != null
+                  && verifyDeveloperPayload(gasYearly));
+          Log.d(TAG, "User "
+              + (mSubscribedToInfiniteGas ? "HAS" : "DOES NOT HAVE")
+              + " infinite gas subscription.");
+          if (mSubscribedToInfiniteGas) mTank = TANK_MAX;
+
+          // Check for gas delivery -- if we own gas, we should fill up the tank immediately
+          Purchase gasPurchase = inventory.getPurchase(Skus.SKU_GAS_ID);
+          if (gasPurchase != null && verifyDeveloperPayload(gasPurchase)) {
+            Log.d(TAG, "We have gas. Consuming it.");
+            try {
+              mHelper.consumeAsync(inventory.getPurchase(Skus.SKU_GAS_ID),
+                  mConsumeFinishedListener);
+            } catch (IabHelper.IabAsyncInProgressException e) {
+              complain("Error consuming gas. Another async operation in progress.");
+            }
+            return;
+          }
+
+          updateUi();
+          setWaitScreen(false);
+          Log.d(TAG, "Initial inventory query finished; enabling main UI.");
+        }
+      };
+
+  // Called when consumption is complete
+  IabHelper.OnConsumeFinishedListener mConsumeFinishedListener =
+      new IabHelper.OnConsumeFinishedListener() {
+        public void onConsumeFinished(Purchase purchase, IabResult result) {
+          Log.d(TAG, "Consumption finished. Purchase: " + purchase + ", result: " + result);
+
+          // if we were disposed of in the meantime, quit.
+          if (mHelper == null) return;
+
+          // We know this is the "gas" sku because it's the only one we consume,
+          // so we don't check which sku was consumed. If you have more than one
+          // sku, you probably should check...
+          if (result.isSuccess()) {
+            // successfully consumed, so we apply the effects of the item in our
+            // game world's logic, which in our case means filling the gas tank a bit
+            Log.d(TAG, "Consumption successful. Provisioning.");
+            mTank = mTank == TANK_MAX ? TANK_MAX : mTank + 1;
+            saveData();
+            alert("You filled 1/4 tank. Your tank is now " + String.valueOf(mTank) + "/4 full!");
+          } else {
+            complain("Error while consuming: " + result);
+          }
+          updateUi();
+          setWaitScreen(false);
+          Log.d(TAG, "End consumption flow.");
+        }
+      };
 
   @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -104,6 +215,49 @@ public class MainActivity extends Activity implements OnClickListener {
 
     // load game data
     loadData();
+
+    /* base64EncodedPublicKey should be YOUR APPLICATION'S PUBLIC KEY
+     * (that you got from your Aptoide's back office). This is not your
+     * developer public key, it's the *app-specific* public key.
+     *
+     * Instead of just storing the entire literal string here embedded in the
+     * program,  construct the key at runtime from pieces or
+     * use bit manipulation (for example, XOR with some other string) to hide
+     * the actual key.  The key itself is not secret information, but we don't
+     * want to make it easy for an attacker to replace the public key with one
+     * of their own and then fake messages from the server.
+     */
+    String base64EncodedPublicKey = BuildConfig.IAB_KEY;
+
+    // Some sanity checks to see if the developer (that's you!) really followed the
+    // instructions to run this sample (don't put these checks on your app!)
+    //if (base64EncodedPublicKey.contains("CONSTRUCT_YOUR")) {
+    //  throw new RuntimeException(
+    //      "Please put your app's public key in MainActivity.java. See README.");
+    //}
+
+    // Create the helper, passing it our context and the public key to verify signatures with
+    Log.d(TAG, "Creating IAB helper.");
+    mHelper = new IabHelper(this, base64EncodedPublicKey);
+
+    // enable debug logging (for a production application, you should set this to false).
+    mHelper.enableDebugLogging(true);
+
+    // Start setup. This is asynchronous and the specified listener
+    // will be called once setup completes.
+    Log.d(TAG, "Starting setup.");
+    mHelper.startSetup(result -> {
+      Log.d(TAG, "Setup finished.");
+
+      if (!result.isSuccess()) {
+        // Oh noes, there was a problem.
+        complain("Problem setting up in-app billing: " + result);
+        return;
+      }
+
+      // Have we been disposed of in the meantime? If so, quit.
+      if (mHelper == null) return;
+    });
 
     updateUi();
   }
@@ -119,9 +273,54 @@ public class MainActivity extends Activity implements OnClickListener {
     super.onActivityResult(requestCode, resultCode, data);
 
     if (appCoinsIab.onActivityResult(requestCode, requestCode, data)) {
-      appCoinsIab.getCurrentPayment()
-          .distinctUntilChanged(PaymentDetails::getPaymentStatus).take(1)
+      final Disposable subscribe = appCoinsIab.getCurrentPayment()
+          .distinctUntilChanged(PaymentDetails::getPaymentStatus)
+          .take(1)
           .subscribe(paymentDetails -> runOnUiThread(() -> handlePayment(paymentDetails)));
+    }
+  }
+
+  @Override public void onClick(DialogInterface dialog, int id) {
+    if (id == 0 /* First choice item */) {
+      mSelectedSubscriptionPeriod = mFirstChoiceSku;
+    } else if (id == 1 /* Second choice item */) {
+      mSelectedSubscriptionPeriod = mSecondChoiceSku;
+    } else if (id == DialogInterface.BUTTON_POSITIVE /* continue button */) {
+
+      if (TextUtils.isEmpty(mSelectedSubscriptionPeriod)) {
+        // The user has not changed from the default selection
+        mSelectedSubscriptionPeriod = mFirstChoiceSku;
+      }
+
+      List<String> oldSkus;
+      if (!TextUtils.isEmpty(mInfiniteGasSku) && !mInfiniteGasSku.equals(
+          mSelectedSubscriptionPeriod)) {
+        // The user currently has a valid subscription, any purchase action is going to
+        // replace that subscription
+        oldSkus = new ArrayList<>();
+        oldSkus.add(mInfiniteGasSku);
+      }
+
+      setWaitScreen(true);
+      Log.d(TAG, "Launching purchase flow for gas subscription.");
+      // TODO: 14-03-2018 neuro subscription
+      // Reset the dialog options
+      mSelectedSubscriptionPeriod = "";
+      mFirstChoiceSku = "";
+      mSecondChoiceSku = "";
+    } else if (id != DialogInterface.BUTTON_NEGATIVE) {
+      // There are only four buttons, this should not happen
+      Log.e(TAG, "Unknown button clicked in subscription dialog: " + id);
+    }
+  }
+
+  @Override public void receivedBroadcast() {
+    // Received a broadcast notification that the inventory of items has changed
+    Log.d(TAG, "Received broadcast notification. Querying inventory.");
+    try {
+      mHelper.queryInventoryAsync(mGotInventoryListener);
+    } catch (IabHelper.IabAsyncInProgressException e) {
+      complain("Error querying inventory. Another async operation in progress.");
     }
   }
 
@@ -186,6 +385,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
   @NonNull private Disposable startBuyFlow(String skuPremiumId) {
     return appCoinsIab.buy(skuPremiumId, this)
+        .observeOn(AndroidSchedulers.mainThread())
         .subscribe(() -> {
           // Buy process successfully started.
           setWaitScreen(false);
@@ -244,40 +444,6 @@ public class MainActivity extends Activity implements OnClickListener {
         .setNegativeButton(R.string.subscription_prompt_cancel, this);
     AlertDialog dialog = builder.create();
     dialog.show();
-  }
-
-  @Override public void onClick(DialogInterface dialog, int id) {
-    if (id == 0 /* First choice item */) {
-      mSelectedSubscriptionPeriod = mFirstChoiceSku;
-    } else if (id == 1 /* Second choice item */) {
-      mSelectedSubscriptionPeriod = mSecondChoiceSku;
-    } else if (id == DialogInterface.BUTTON_POSITIVE /* continue button */) {
-
-      if (TextUtils.isEmpty(mSelectedSubscriptionPeriod)) {
-        // The user has not changed from the default selection
-        mSelectedSubscriptionPeriod = mFirstChoiceSku;
-      }
-
-      List<String> oldSkus;
-      if (!TextUtils.isEmpty(mInfiniteGasSku) && !mInfiniteGasSku.equals(
-          mSelectedSubscriptionPeriod)) {
-        // The user currently has a valid subscription, any purchase action is going to
-        // replace that subscription
-        oldSkus = new ArrayList<>();
-        oldSkus.add(mInfiniteGasSku);
-      }
-
-      setWaitScreen(true);
-      Log.d(TAG, "Launching purchase flow for gas subscription.");
-      // TODO: 14-03-2018 neuro subscription
-      // Reset the dialog options
-      mSelectedSubscriptionPeriod = "";
-      mFirstChoiceSku = "";
-      mSecondChoiceSku = "";
-    } else if (id != DialogInterface.BUTTON_NEGATIVE) {
-      // There are only four buttons, this should not happen
-      Log.e(TAG, "Unknown button clicked in subscription dialog: " + id);
-    }
   }
 
   // Drive button clicked. Burn gas!
@@ -354,5 +520,35 @@ public class MainActivity extends Activity implements OnClickListener {
     mTank = sp.getInt("tank", 2);
     mIsPremium = sp.getBoolean("mIsPremium", mIsPremium);
     Log.d(TAG, "Loaded data: tank = " + String.valueOf(mTank));
+  }
+
+  /** Verifies the developer payload of a purchase. */
+  boolean verifyDeveloperPayload(Purchase p) {
+    String payload = p.getDeveloperPayload();
+
+    /*
+     * TODO: verify that the developer payload of the purchase is correct. It will be
+     * the same one that you sent when initiating the purchase.
+     *
+     * WARNING: Locally generating a random string when starting a purchase and
+     * verifying it here might seem like a good approach, but this will fail in the
+     * case where the user purchases an item on one device and then uses your app on
+     * a different device, because on the other device you will not have access to the
+     * random string you originally generated.
+     *
+     * So a good developer payload has these characteristics:
+     *
+     * 1. If two different users purchase an item, the payload is different between them,
+     *    so that one user's purchase can't be replayed to another user.
+     *
+     * 2. The payload must be such that you can verify it even when the app wasn't the
+     *    one who initiated the purchase flow (so that items purchased by the user on
+     *    one device work on other devices owned by the user).
+     *
+     * Using your own server to store and verify developer payloads across app
+     * installations is recommended.
+     */
+
+    return true;
   }
 }
