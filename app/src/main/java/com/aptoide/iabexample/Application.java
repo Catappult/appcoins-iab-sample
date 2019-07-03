36429244d47a7425e1294ea3473b7103f1918c90
Bruno Varela <bruno.varela@aptoide.com>
2019-07-03 10:24:27 +0100

    Classes added
     the IAB Classes

---
+++ app/src/main/java/com/aptoide/iabexample/Application.java
@@ -0,0 +1,30 @@
+package com.aptoide.iabexample;
+
+import android.content.pm.PackageManager;
+import com.asf.appcoins.sdk.ads.AppCoinsAds;
+import com.asf.appcoins.sdk.ads.AppCoinsAdsBuilder;
+
+/**
+ * Created by neuro on 12-03-2018.
+ */
+
+public class Application extends android.app.Application {
+  private final String developerAddress = BuildConfig.IAB_WALLET_ADDR;
+
+  private static AppCoinsAds adsSdk;
+
+  @Override public void onCreate() {
+    super.onCreate();
+    adsSdk = new AppCoinsAdsBuilder().withDebug(BuildConfig.TEST_NETWORK)
+        .createAdvertisementSdk(this);
+    try {
+      adsSdk.init(this);
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public String getDeveloperAddress() {
+    return developerAddress;
+  }
+}
